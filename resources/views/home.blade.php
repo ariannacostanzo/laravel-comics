c4450de2aacdc01d7265f1d5c1e3a1f22086b85f
ariannacostanzo <costanzo.arianna@outlook.it>
2024-03-07 14:03:45 +0100

    creating header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,60 @@
+<?php
+
+$header_nav = [
+    [
+        'text' => 'Characters',
+        'url' => '#',
+        'current' => false,
+    ],
+    [
+        'text' => 'Comics',
+        'url' => '#',
+        'current' => true,
+    ],
+    [
+        'text' => 'Movies',
+        'url' => '#',
+        'current' => false,
+    ],
+    [
+        'text' => 'TV',
+        'url' => '#',
+        'current' => false,
+    ],
+    [
+        'text' => 'Games',
+        'url' => '#',
+        'current' => false,
+    ],
+    [
+        'text' => 'Collectibles',
+        'url' => '#',
+        'current' => false,
+    ],
+    [
+        'text' => 'Videos',
+        'url' => '#',
+        'current' => false,
+    ],
+    [
+        'text' => 'Fans',
+        'url' => '#',
+        'current' => false,
+    ],
+    [
+        'text' => 'News',
+        'url' => '#',
+        'current' => false,
+    ],
+    [
+        'text' => 'Shop',
+        'url' => '#',
+        'current' => false,
+    ],
+];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +65,24 @@
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    home
+    {{-- header --}}
+    <header>
+        <div class="header container">
+            <figure>
+                <img src="../assets/images/dc-logo.png" alt="">
+            </figure>
+            <nav>
+                <ul>
+                    @foreach ($header_nav as $nav)
+                        
+                    <li><a href="{{$nav['url']}}" class="{{$nav['current'] ? 'active' : ''}}">{{$nav['text']}}</a></li>
+                    @endforeach
+                </ul>
+            </nav>
+        </div>
+    </header>
+    {{-- header --}}
+
+    
 </body>
 </html>
